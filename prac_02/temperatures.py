41c64eacbf5ffcc8cacba51b7d838b94723b664d
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-21 15:35:33 +1000

    Added main function

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -3,12 +3,28 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = get_fahrenheit(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = get_celsius(fahrenheit)
+            print("Result: {:.2f} C".format(celsius))
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
 def get_fahrenheit(celsius):
     return celsius * 9.0 / 5 + 32
@@ -18,18 +34,4 @@ def get_celsius(fahrenheit):
     return 5 / 9 * (fahrenheit - 32)
 
 
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = get_fahrenheit(celsius)
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = get_celsius(fahrenheit)
-        print("Result: {:.2f} C".format(celsius))
-
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+main()
