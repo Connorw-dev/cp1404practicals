6dfb43e582fb89a6f3be9f4ea6c5bf3a7b2ce68b
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-21 15:42:27 +1000

    Refactored score program with functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,27 @@
 CP1404/CP5632 - Practical
 Program to determine score status.
 """
+from random import randint
 
-score = float(input("Enter score: "))
 
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score >= 0:
-    print("Bad")
-else:
-    print("Invalid score")
+def main():
+    score = float(input("Enter score: "))
+    print(get_result(score))
+
+    random_score = randint(0, 100)
+    print(f"Random score: {random_score}")
+    print(get_result(random_score))
+
+
+def get_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score >= 0:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
