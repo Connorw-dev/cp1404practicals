4b59ab914bf81d13cfd68164437753aba5feb0d0
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-21 15:33:23 +1000

    Refactor temperatures program with functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,14 +8,24 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def get_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def get_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = get_fahrenheit(celsius)
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = get_celsius(fahrenheit)
         print("Result: {:.2f} C".format(celsius))
 
     else:
