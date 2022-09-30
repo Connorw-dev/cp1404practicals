f727aef2cc26facc578f6cef5c960323f8c3564a
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-30 13:41:14 +1000

    Add number_of_days variable to capitalist_conrad program

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -18,7 +18,9 @@ INITIAL_PRICE = 10.0
 price = INITIAL_PRICE
 print("${:,.2f}".format(price))
 
+number_of_days = 0
 while price >= MIN_PRICE and price <= MAX_PRICE:
+    number_of_days += 1
     price_change = 0
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
@@ -32,4 +34,4 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print("${:,.2f}".format(price))
+    print("On day {:,} price is: ${:,.2f}".format(number_of_days, price))
