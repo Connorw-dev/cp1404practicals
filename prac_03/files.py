96b30bd67743494141d5853af5a844545dd7299f
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-30 14:17:49 +1000

    Refactored constant names in files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,13 +2,17 @@
 CP1404/CP5632 - Practical
 Experiment with reading files
 """
-FILE_NAME = "names.txt"
+NAME_FILE = "names.txt"
+NUMBERS_FILE = "numbers.txt"
 
 name = input("Name: ")
 
-with open(FILE_NAME, "w") as out_file:
-    print(name, file=out_file)
+with open(NAME_FILE, "w") as name_file:
+    print(name, file=name_file)
 
-with open(FILE_NAME, "r") as name_file:
+with open(NAME_FILE, "r") as name_file:
     print(f"Your name is {name_file.read()}")
 
+with open(NUMBERS_FILE, "r") as numbers_file:
+    lines = numbers_file.readlines()
+    print(lines)
