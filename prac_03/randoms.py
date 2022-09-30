8e152f0f215c7a4b2d85ff8effb2528b23bc6108
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-09-30 13:36:42 +1000

    Add comments to randoms program answering questions, add random_number variable

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 - Practical 3
+"""
+import random
+
+# Question 1: 5, 19
+# Question 2: 3, 9
+# Question 3: It could not have produced a 4
+# Question 4: I saw float numbers
+# Question 5: smallest is 2.5, largest is 5.4999...
+
+random_number = random.randrange(1, 100+1)
