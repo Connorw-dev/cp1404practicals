9802f567c547029950f517500b9956d3b3435778
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-02 10:47:07 +1000

    Update randoms.py

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -5,10 +5,10 @@ Experiment with random
 
 import random
 
-# Question 1: 5, 19
+# Question 1: 5, 20
 # Question 2: 3, 9
 # Question 3: It could not have produced a 4
 # Question 4: I saw float numbers
 # Question 5: smallest is 2.5, largest is 5.4999...
 
-random_number = random.randrange(1, 100+1)
+random_number = random.randint(1, 100)
