682f156696f709a78191ff68b13b5aff2aee16bb
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-07 11:43:44 +1000

    Modify list_exercises.py to get user input and print stats

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Practical
+Program to display stats about numbers
+"""
+
+NUMBER_COUNT = 5
+
+
+def main():
+    numbers = [int(input("Number: ")) for i in range(NUMBER_COUNT)]
+    print(f"The first number is: {numbers[0]}")
+    print(f"The last number is: {numbers[-1]}")
+    print(f"The smallest number is: {min(numbers)}")
+    print(f"The largest number is: {max(numbers)}")
+    print(f"The average number is: {sum(numbers)/len(numbers)}")
+
+
+if __name__ == "__main__":
+    main()
