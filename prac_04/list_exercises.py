a8c47b35ea9093dbc1d9cbb63acf761b4c63eb69
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-07 11:50:59 +1000

    Add username check to list_exercises.py, refactor print_stats function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,15 +4,27 @@ Program to display stats about numbers
 """
 
 NUMBER_COUNT = 5
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 
 def main():
+    username = input("Username: ")
+    if username in USERNAMES:
+        print("Access granted")
+    else:
+        print("Access denied")
+
     numbers = [int(input("Number: ")) for _ in range(NUMBER_COUNT)]
+    print_stats(numbers)
+
+
+def print_stats(numbers):
     print(f"The first number is: {numbers[0]}")
     print(f"The last number is: {numbers[-1]}")
     print(f"The smallest number is: {min(numbers)}")
     print(f"The largest number is: {max(numbers)}")
-    print(f"The average number is: {sum(numbers)/len(numbers)}")
+    print(f"The average number is: {sum(numbers) / len(numbers)}")
 
 
 if __name__ == "__main__":
