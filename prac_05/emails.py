f153e47c657768f3e1dfa98ac861df8f6f015f62
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-15 13:35:28 +1000

    Complete emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,32 @@
 """
 CP1404/CP5632 Practical
-Emails
+Emails in dictionaries
 """
 
+
+def main():
+    """ Get email and name from user and store in a dictionary"""
+    email_dict = {}
+
+    email = input("Email: ")
+    while email:
+        name = get_name(email)
+        name_correct_input = input(f"Is your name {name}? (Y/n) ").lower()
+        if name_correct_input and name_correct_input != "y":
+            name = input("Name: ")
+        email_dict[email] = name
+        email = input("Email: ")
+
+    for email, name in email_dict.items():
+        print(f"{name} ({email})")
+
+
+def get_name(email):
+    """ Extract name from email address """
+    email_prefix = email.split('@')[0]
+    words = email_prefix.split('.')
+    return ' '.join([word.title() for word in words])
+
+
+if __name__ == "__main__":
+    main()
