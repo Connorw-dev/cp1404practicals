8aa980f7463653bef6c7eb81497abb219928da09
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-15 13:13:26 +1000

    Complete word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,20 @@ CP1404/CP5632 Practical
 Word occurrences in a string
 """
 
+
+def main():
+    words = input("String: ").split()
+    word_dict = {}
+    for word in words:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    max_word_length = max([len(word) for word in word_dict])
+    for word, count in sorted(word_dict.items()):
+        print(f"{word:{max_word_length}} : {count}")
+
+
+if __name__ == "__main__":
+    main()
