c131a61373126fc6cb0d6fe69a2ae7681bc8fe96
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-31 13:19:33 +1000

    Complete guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical - Client code to use the Guitar class.
 """
 from prac_06.guitar import Guitar
 
-guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40)]
+guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
 
-for guitar in guitars:
-    print(guitar)
+print(guitar)
+print(f"Age: {guitar.get_age()}, Vintage? {guitar.is_vintage()}")
