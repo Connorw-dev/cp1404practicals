077b45016673e9b900e1fe8ca7d95feca7816b9b
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-10-31 13:12:16 +1000

    Add guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,20 @@
+"""CP1404/CP5632 Practical - Guitar class"""
+import datetime
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,}"
+
+    def get_age(self):
+        return datetime.date.today().year - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
