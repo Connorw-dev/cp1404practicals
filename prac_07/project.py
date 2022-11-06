c8df1f726cd3daa8f33b9e432d13168e9cf013b2
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-11-06 18:42:53 +1000

    Add menu and initial class

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,3 +2,15 @@
 CP1404/CP5632 Practical
 Project class
 """
+
+
+class Project:
+    """Represent information about a Project."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,3 +3,35 @@ Program to load and save a data file and use a list of Project objects.
 Estimated time: 30m
 Actual time:
 """
+
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit
+>>> """
+
+
+def main():
+    choice = input(MENU).lower()
+    while choice != "q":
+        if choice == "l":
+            projects = load_projects()
+        elif choice == "s":
+            save_projects(projects)
+        elif choice == "d":
+            display_projects(projects)
+        elif choice == "f":
+            filter_projects(projects)
+        elif choice == "a":
+            add_project(projects)
+        elif choice == "u":
+            update_project(project)
+        choice = input(MENU).lower()
+    return
+
+
+if __name__ == "__main__":
+    main()
