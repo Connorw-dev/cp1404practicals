1742ab45350921824f69052c9fc310088ea9f688
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-11-06 19:01:26 +1000

    Add load_projects, save_projects and display_projects functions

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,7 @@ Program to load and save a data file and use a list of Project objects.
 Estimated time: 30m
 Actual time:
 """
+from project import Project
 
 MENU = """- (L)oad projects  
 - (S)ave projects  
@@ -32,6 +33,39 @@ def main():
         choice = input(MENU).lower()
     return
 
+
+def load_projects():
+    projects = []
+    projects_file = input("Projects filename: ")
+    with open(projects_file) as in_file:
+        # Consume CSV header
+        in_file.readline()
+
+        for line in in_file:
+            parts = line.strip().split('\t')  # TAB delimiter
+            start_date = parts[1]
+            priority = int(parts[2])
+            cost_estimate = float(parts[3])
+            completion_percentage = int(parts[4])
+            projects.append(Project(parts[0], start_date, priority, cost_estimate, completion_percentage))
+    return projects
+
+
+def save_projects(projects):
+    projects_file = input("Projects filename: ")
+    with open(projects_file, "w") as out_file:
+        for project in projects:
+            print(project.name, project.start_date, project.priority,
+                  project.cost_estimate, project.completion_percentage, sep="\t", file=out_file)
+
+
+def display_projects(projects):
+    completed_projects = [project for project in projects if project.completion_percentage == 100]
+    incomplete_projects = list(set(projects) - set(completed_projects))
+    print("Incomplete projects:")
+    [print(project, end="\n") for project in incomplete_projects]
+    print("Completed projects:")
+    [print(project, end="\n") for project in completed_projects]
 
 if __name__ == "__main__":
     main()
