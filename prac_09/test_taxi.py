6557d297932194992f50cb58e510b6f518368d2c
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2022-11-21 12:23:11 +1000

    Complete Taxi Class

---
+++ prac_09/test_taxi.py
@@ -0,0 +1,9 @@
+""" CP1404 Practical 9 - Inheritance """
+from taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100)
+my_taxi.drive(40)
+print(my_taxi)
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,4 @@
+""" CP1404 Practical 9 - Unreliable Car"""
+from prac_09.car import Car
+
+class UnreliableCar(Car):
